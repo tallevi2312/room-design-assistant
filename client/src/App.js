fe2bf2c98aca0fda050935b694cbaa5f2b417158
Tal Levi <45764529+tallevi2312@users.noreply.github.com>
2026-03-16 19:01:37 +0200

    Create main React app component

---
+++ client/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Welcome to the Room Design Assistant!</h1>
+      <p>This is your main React app for designing and arranging rooms.</p>
+    </div>
+  );
+}
+
+export default App;
