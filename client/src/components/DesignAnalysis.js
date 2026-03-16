384d52a0cad7bbd2783e10a4089f98324fbee4b4
Tal Levi <45764529+tallevi2312@users.noreply.github.com>
2026-03-16 19:06:30 +0200

    Create DesignAnalysis.js component

---
+++ client/src/components/DesignAnalysis.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const DesignAnalysis = () => {
+    return (
+        <div>
+            <h1>Design Analysis Component</h1>
+            <p>This component is responsible for analyzing room designs based on user inputs.</p>
+        </div>
+    );
+};
+
+export default DesignAnalysis;
