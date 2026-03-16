a55c05a8be5115c3d3a0e59d1ceaa604b7d86c0a
Tal Levi <45764529+tallevi2312@users.noreply.github.com>
2026-03-16 19:06:36 +0200

    Create MaterialRecommendations.js component

---
+++ client/src/components/MaterialRecommendations.js
@@ -0,0 +1,14 @@
+// MaterialRecommendations.js
+
+import React from 'react';
+
+const MaterialRecommendations = () => {
+    return (
+        <div>
+            <h1>Material Recommendations</h1>
+            {/* Add your material recommendation logic here */}
+        </div>
+    );
+};
+
+export default MaterialRecommendations;
