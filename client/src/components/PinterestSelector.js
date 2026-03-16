f28ad7584d5003f615d6cac881340f1878092d79
Tal Levi <45764529+tallevi2312@users.noreply.github.com>
2026-03-16 19:06:19 +0200

    Create PinterestSelector.js component

---
+++ client/src/components/PinterestSelector.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const PinterestSelector = () => {
+    return (
+        <div>
+            <h1>Select Images from Pinterest</h1>
+            {/* Implement your Pinterest selection functionality here */}
+        </div>
+    );
+};
+
+export default PinterestSelector;
